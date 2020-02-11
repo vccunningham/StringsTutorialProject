8c04019be41496ca1c4fe49820fbc505b4345a3c
max-student <max-student@maxtrain.com>
2020-02-11 12:24:40 -0500

    Add project files.

---
+++ StringsTutorialProject/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace StringsTutorialProject {
+    class Program {
+        static void Main(string[] args) {
+
+            var st1 = new StringsTutorial();
+            var fullname = st1.Fullname();
+            Console.WriteLine($"Fullname is {fullname}");
+            Console.WriteLine($"Fullname2 is {st1.Fullname2()}");
+
+            var denise = new StringsTutorial("Denise", "Bartrick");
+            Console.WriteLine($"Denise {denise.Fullname()}");
+            Console.WriteLine($"Denise {denise.Fullname2()}");
+            }
+
+        }
+    }
---
+++ StringsTutorialProject/StringsTutorial.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StringsTutorialProject {
+    class StringsTutorial {
+
+
+        public string Firstname;
+        public string Lastname;
+
+        public StringsTutorial() {
+            Firstname = "Vaughn";
+            Lastname = "Cunningham";
+
+            }
+        public StringsTutorial(string firstname, string lastname) {
+            Firstname = firstname;
+            Lastname = lastname;
+            }
+        
+
+
+
+        public string Fullname() {
+            //var Firstname = "Vaughn";
+            //var Lastname = "Cunningham";
+
+            //var fullname = Firstname + " " + Lastname;
+            var fullname = $"{Lastname}, {Firstname}";
+            return fullname;
+
+            }
+
+
+        public string Fullname2() {
+            //var Firstname = "Vaughn";
+            //var Lastname = "Cunningham";
+
+            var fullname = $"{Firstname} {Lastname}";
+            return fullname;
+
+            }
+
+        }
+    }
